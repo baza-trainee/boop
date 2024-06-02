4a023eb81a78bcb6648b6a9ae0cde682c38ba77b
Богдан <sysoevbogdan82@gmail.com>
2024-06-02 13:02:41 +0300

    reafctor/added wrapper
    added wrapper. Added styles for class lock. Changed useBodyLockScroll

--- src/hooks/useBodyScrollLock.ts
+++ src/hooks/useBodyScrollLock.ts
@@ -4,18 +4,21 @@ import { useEffect } from 'react';
 
 export const useBodyScrollLock = (shouldLock: boolean) => {
   useEffect(() => {
-    const originalOverflow = window.getComputedStyle(
-      document.body
-    ).overflow;
-    if (shouldLock) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = originalOverflow;
-      document.body.style.overflowX = 'hidden';
+    if (!shouldLock) {
+      return;
     }
+
+    const scrollBarWidth =
+      window.innerWidth - document.documentElement.clientWidth;
+
+    document.body.style.paddingRight = `${scrollBarWidth}px`;
+
+    document.body.classList.add('lock');
+
     return () => {
-      document.body.style.overflow = originalOverflow;
-      document.body.style.overflowX = 'hidden';
+      document.body.classList.remove('lock');
+
+      document.body.style.paddingRight = '0px';
     };
   }, [shouldLock]);
 };
