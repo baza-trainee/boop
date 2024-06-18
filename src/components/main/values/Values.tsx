6d1da3d21f78d15f7e5a5790180f3846f8bfe300
VickyBondarenko <quikyv@gmail.com>
2024-06-18 20:00:09 +0200

    Added hover for valueCards

--- src/components/main/values/Values.tsx
+++ src/components/main/values/Values.tsx
@@ -37,12 +37,15 @@ type ValueCardProps = {
   text: string;
   icon: string;
   hoverText: string;
+  index: number;
 };
 
-const ValueCard = ({ text, icon, hoverText }: ValueCardProps) => {
+const ValueCard = ({ text, icon, hoverText, index }: ValueCardProps) => {
   return (
-    <div className="flex flex-col gap-2">
-      <div className="flex gap-4">
+    <div className="group relative flex w-[251px] cursor-pointer flex-col gap-2">
+      <div
+        className={`flex gap-4 pb-2 ${index === 2 || index === 3 ? `flex-row-reverse justify-end` : ``}`}
+      >
         <p className="title-gradient p-0 font-groppled text-xl font-bold leading-none">
           {text}
         </p>
@@ -51,7 +54,7 @@ const ValueCard = ({ text, icon, hoverText }: ValueCardProps) => {
         </svg>
       </div>
 
-      <div className="relative w-[248px] rounded-2xl border-0 border-inherit bg-bgWhite p-4 shadow-[0_2px_6px_2px_rgba(47,36,94,0.15)] before:absolute before:-inset-0 before:block before:rotate-[-1deg] before:rounded-2xl before:border-[1px] before:border-yellow">
+      <div className="invisible absolute top-[100%] w-[248px] rounded-2xl border-0 border-inherit bg-bgWhite p-4 shadow-[0_2px_6px_2px_rgba(47,36,94,0.15)] before:absolute before:-inset-0 before:block before:rotate-[-1deg] before:rounded-2xl before:border-[1px] before:border-yellow group-hover:visible ">
         <p className="text-left font-raleway text-[14px] leading-[116%] text-textViolet">
           {hoverText}
         </p>
@@ -62,8 +65,8 @@ const ValueCard = ({ text, icon, hoverText }: ValueCardProps) => {
 
 const Values = () => {
   return (
-    <section className=" bg-bgYellow  w-full    px-[120px]   pb-[48px] pt-[55px] ">
-      <ul className="flex flex-wrap items-center justify-center gap-x-[276px]  gap-y-[-10px]">
+    <section className=" bg-bgYellow  w-full    px-[120px]   pb-[96px] pt-[55px] ">
+      <ul className="flex flex-wrap items-center justify-center gap-x-[271px]  gap-y-12">
         {valuesCards.map((el, index) => {
           return (
             <li key={index}>
@@ -71,6 +74,7 @@ const Values = () => {
                 text={el.text}
                 icon={el.icon}
                 hoverText={el.hoverText}
+                index={index}
               />
             </li>
           );
