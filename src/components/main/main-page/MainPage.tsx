9f35d335b1bd57bda47d6be5b37545ae6ee05f49
VickyBondarenko <quikyv@gmail.com>
2024-06-18 13:00:06 +0200

    Added vision section

--- src/components/main/main-page/MainPage.tsx
+++ src/components/main/main-page/MainPage.tsx
@@ -2,10 +2,11 @@
 
 import Image from 'next/image';
 import SectionTitle from '../shared/SectionTitle';
+import Vision from '../vision/Vision';
 
 const MainPage = () => {
   return (
-    <div className="relative flex min-h-[100vh] w-full items-center justify-center bg-bgWhite">
+    <div className="colou relative flex min-h-[100vh] w-full flex-col items-center justify-center bg-bgWhite">
       <Image
         src="/images/hero-bg-image.svg"
         alt=""
@@ -14,6 +15,7 @@ const MainPage = () => {
         className="-z-1 absolute -top-[100px] right-0"
       />
       <SectionTitle title="Example Title" />
+      <Vision />
     </div>
   );
 };
---
+++ src/components/main/vision/Vision.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Vision = () => {
+  return (
+    <section className=" w-[1200px] flex-col items-center justify-center gap-6  px-[77px] pb-6 text-center">
+      <h3 className="font-raleway text-xl font-bold  leading-[132%] text-textViolet">
+        Візія
+      </h3>
+      <h2 className="title-gradient font-groppled text-5xl font-bold leading-[124%]">
+        Ми прагнемо, аби в кожній дитячій лікарні України було місце для радості
+        і посмішок дітей, які опинилися в складних та кризових ситуаціях.
+      </h2>
+    </section>
+  );
+};
+
+export default Vision;
