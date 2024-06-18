3f4509be5375757ef0bd18431c226e558a39882e
Богдан <sysoevbogdan82@gmail.com>
2024-06-18 22:53:47 +0300

    feature/counter
    Finished counter section. Added all icons I used to sprite.

--- src/components/main/main-page/counter/Counter.tsx
+++ src/components/main/main-page/counter/Counter.tsx
@@ -10,9 +10,14 @@ const Counter = () => {
         <SectionTitle title={'Досягнення в цифрах'} />
       </div>
       <div className="mt-8 bg-beige pb-[75px] pt-[75px]">
-        <ul className="mx-auto flex max-w-[1075px] justify-between gap-[20px]">
+        <ul className="mx-auto flex max-w-[1296px] flex-wrap justify-between gap-[20px] [&>*:nth-child(n+2):nth-child(-n+4)]:text-yellow">
           {counterItems?.map(({ id, number, text }) => (
-            <CounterItem key={id} number={number} text={text} />
+            <CounterItem
+              key={id}
+              number={number}
+              text={text}
+              variant={`${id}`}
+            />
           ))}
         </ul>
       </div>
--- src/components/main/main-page/counter/counter-item/CounterItem.tsx
+++ src/components/main/main-page/counter/counter-item/CounterItem.tsx
@@ -4,19 +4,94 @@ import CountUp from 'react-countup';
 interface CounterItemProps {
   number: number;
   text: string;
+  variant?: string;
 }
 
-const CounterItem = ({ number, text }: CounterItemProps) => {
+const CounterItem = ({ number, text, variant = '1' }: CounterItemProps) => {
   return (
-    <li className="font-groppleds relative h-[183px] odd:text-yellow even:text-violet">
-      <div className="absolute left-0 top-0 h-[180px] w-[180px]"></div>
-      <div className="max-w-[143px] pt-9 text-center font-bold">
-        <div className="mb-2 text-5xl leading-[1.2]">
-          <CountUp start={0} end={number} duration={3} enableScrollSpy />
+    <>
+      <li className="font-groppleds relative h-[183px] first:text-violet last:text-violet">
+        <div className="relative">
+          {variant === '1' && (
+            <>
+              <svg className="h-[180px] w-[187px]">
+                <use href="/icons/sprite.svg#counter-circle-yellow"></use>
+              </svg>
+              <svg className="absolute bottom-[2px] left-[2px] h-[182px] w-[191px]">
+                <use href="/icons/sprite.svg#counter-border-violet"></use>
+              </svg>
+              <svg className="absolute -top-[23px] right-[42px] h-[62px] w-[56px]">
+                <use href="/icons/sprite.svg#bow-violet"></use>
+              </svg>
+            </>
+          )}
+          {variant === '2' && (
+            <>
+              <svg className="h-[179px] w-[180px]">
+                <use href="/icons/sprite.svg#counter-circle-red"></use>
+              </svg>
+              <svg className="absolute bottom-[4px] left-[5px] h-[181px] w-[182px]">
+                <use href="/icons/sprite.svg#counter-border-yellow"></use>
+              </svg>
+              <svg className="absolute -top-[39px] left-[4px] h-[74px] w-[60px]">
+                <use href="/icons/sprite.svg#party-hat-violet"></use>
+              </svg>
+            </>
+          )}
+          {variant === '3' && (
+            <>
+              <svg className="h-[173px] w-[180px]">
+                <use href="/icons/sprite.svg#counter-circle-violet"></use>
+              </svg>
+              <svg className="absolute bottom-[4px] right-[4px] h-[175px] w-[182px]">
+                <use href="/icons/sprite.svg#counter-border-red"></use>
+              </svg>
+              <svg className="absolute -top-[36px] right-[11px] h-[78px] w-[95px]">
+                <use href="/icons/sprite.svg#cap"></use>
+              </svg>
+            </>
+          )}
+          {variant === '4' && (
+            <>
+              <svg className="h-[179px] w-[180px]">
+                <use href="/icons/sprite.svg#counter-circle-red"></use>
+              </svg>
+              <svg className="absolute bottom-[4px] left-[5px] h-[181px] w-[182px]">
+                <use href="/icons/sprite.svg#counter-border-yellow"></use>
+              </svg>
+              <svg className="absolute -top-[39px] right-[20px] h-[74px] w-[61px]">
+                <use href="/icons/sprite.svg#party-hat-yellow"></use>
+              </svg>
+            </>
+          )}
+          {variant === '5' && (
+            <>
+              <svg className="h-[180px] w-[187px]">
+                <use href="/icons/sprite.svg#counter-circle-yellow"></use>
+              </svg>
+              <svg className="absolute bottom-[2px] left-[2px] h-[182px] w-[191px]">
+                <use href="/icons/sprite.svg#counter-border-violet"></use>
+              </svg>
+              <svg className="absolute -top-[13px] right-[29px] h-[44px] w-[56px]">
+                <use href="/icons/sprite.svg#bow-red"></use>
+              </svg>
+            </>
+          )}
         </div>
-        <div className="text-xl leading-[1] text-textViolet">{text}</div>
-      </div>
-    </li>
+        <div className="absolute left-[50%] top-0 w-[143px] -translate-x-2/4 pt-9 text-center font-bold">
+          <div className="mb-2 text-5xl leading-[1.2]">
+            <CountUp
+              start={0}
+              end={number}
+              duration={3}
+              enableScrollSpy
+              scrollSpyOnce={true}
+            />
+          </div>
+          <div className="text-xl leading-[1] text-textViolet">{text}</div>
+        </div>
+      </li>
+    </>
   );
 };
 
