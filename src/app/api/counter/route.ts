041deb2cc6c7b3d4938077c9e0c4b8fb6883e303
Oleh Hubskiy <gubskyolegod@gmail.com>
2024-06-22 14:33:23 +0300

    create counter-endpoint

---
+++ src/app/api/counter/route.ts
@@ -0,0 +1,36 @@
+import prisma from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+import { prismaConnect } from '@/utils/prismaConnect';
+import { CounterFormData } from '@/types/counterItem';
+
+import './swagger-comments';
+
+export async function GET() {
+  try {
+    await prismaConnect();
+    const response = await prisma.counterItem.findMany();
+    response.sort((a, b) => a.order - b.order);
+    return NextResponse.json(response, { status: 200 });
+  } catch (error) {
+    console.log('[GET COUNTER]', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await prismaConnect();
+    const data: CounterFormData = await request.json();
+    const response = await prisma.counterItem.create({
+      data: {
+        number: data.number,
+        text: data.text,
+        variant: data.variant,
+      },
+    });
+    return NextResponse.json(response, { status: 200 });
+  } catch (error) {
+    console.log('ADD COUNTER', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
