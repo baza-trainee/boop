f1aabee738eba163558e03bc9ad3bef87fa14ac0
Torianyk <an.mol4anova@gmail.com>
2024-06-23 18:44:57 +0300

    Added header elements for 1536

--- src/components/main/header/Header.tsx
+++ src/components/main/header/Header.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { usePathname } from 'next/navigation';
+import Image from 'next/image';
 
 const Header = () => {
   const pathname = usePathname();
@@ -10,8 +11,41 @@ const Header = () => {
   if (isAdminPage) return null;
 
   return (
-    <div className="container flex h-[100px] w-full items-center bg-beige text-violet">
-      Header
+    <div className="container flex h-[100px] w-full items-center justify-between bg-beige text-violet">
+      <div className="w-1/5">
+        <Image src="/images/logo.svg" alt="logo" width={86} height={50} />
+      </div>
+      <nav className="flex w-3/5 justify-end overflow-y-auto whitespace-nowrap pr-11 font-groppled text-xl font-bold leading-5">
+        <ul className="flex items-center">
+          <li className="pl-4 pr-5">
+            <a href="#">Головна</a>
+          </li>
+          <li className="pl-4 pr-5">
+            <a href="#">Про нас</a>
+          </li>
+          <li className="pl-4 pr-5">
+            <a href="#">Школа Клоунів</a>
+          </li>
+          <li className="pl-4 pr-5">
+            <a href="#">Контакти</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="-ml-5 flex w-1/5 items-center justify-end pl-8 font-raleway ">
+        <button className="flex items-center pr-2 font-semibold">
+          UA
+          <Image
+            className="w-6"
+            src="/images/keyboard_arrow_down.svg"
+            alt="logo"
+            width={86}
+            height={50}
+          />
+        </button>
+        <button className="-mr-11 ml-1 whitespace-nowrap rounded-full bg-red px-4 py-3 text-xl font-bold leading-5 text-white">
+          Підтримати проєкт
+        </button>
+      </div>
     </div>
   );
 };
