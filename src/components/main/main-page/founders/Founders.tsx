ebd159c8a969a8af8ec0ffaf3a255844f311dba2
VickyBondarenko <quikyv@gmail.com>
2024-06-29 17:02:51 +0300

    Added eng translation to founders section

--- src/components/main/main-page/founders/Founders.tsx
+++ src/components/main/main-page/founders/Founders.tsx
@@ -5,35 +5,32 @@ import { useLocale, useTranslations } from 'next-intl';
 
 const Founders = () => {
   const t = useTranslations('Founders');
-  //   const locale = useLocale();
+  const locale = useLocale();
 
   return (
-    <section className="container  mb-[120px]  flex   items-center justify-between bg-inherit ">
+    <section className="container  mb-[120px]  flex   items-center justify-between whitespace-pre bg-inherit">
       <div className=" flex items-end gap-[2px]">
         <Image
           src="/images/founders_image.png"
-          alt=""
+          alt="Founders foto"
           width={636}
           height={724}
           className=" "
         />
         <span className=" relative -bottom-4  h-4 w-[163px] origin-top-left -rotate-90 text-left font-raleway text-[12px] font-semibold leading-[132%] text-textViolet">
-          Бюро усмішок та підтримки
+          {t('caption')}
         </span>
       </div>
 
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col ">
         <h3 className="font-raleway text-xl font-[500]  leading-[132%] text-mainViolet">
-          {/* {t('subtitle')} */}
-          Засновниці проєкту
+          {t('subtitle')}
         </h3>
-        <h2 className="title-gradient w-[458px] pb-3 font-groppled text-5xl font-bold leading-[110%]">
-          {/* {t('text')} */}
-          Ольга Булкіна та Марина Бердар
+        <h2 className="title-gradient w-[500px] pb-4 font-groppled text-5xl font-bold leading-[110%]">
+          {t('title')}
         </h2>
-        <p className="w-[300px] font-raleway text-xl font-bold leading-[132%] text-textViolet">
-          Дипломовані лікарняні клоунеси. Тренерки. Почесні членкині організації
-          ”SOCCORSO CLOWN”
+        <p className="w-[350px] font-raleway text-xl font-bold leading-[132%] text-textViolet">
+          {t('text')}
         </p>
       </div>
     </section>
