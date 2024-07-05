87f8d96d5c1c62c5398524c7980cf0599b4d6233
Богдан <sysoevbogdan82@gmail.com>
2024-07-05 17:43:24 +0300

    feat: finished feedback section

--- src/components/main/school/components/feedback/Feedback.tsx
+++ src/components/main/school/components/feedback/Feedback.tsx
@@ -3,12 +3,20 @@
 import { Carousel } from '@/components/main/shared/carousel/Carousel';
 import CarouselButton from '@/components/main/shared/carousel/carousel-button/CarouselButton';
 import React from 'react';
-import { items } from './items';
 import FeedbackItem from './feedback-item/FeedbackItem';
 import SectionTitle from '@/components/main/shared/SectionTitle';
 import Image from 'next/image';
+import { useGetAllTestimonialsQuery } from '@/store/api/testimonialsApi';
+import { useParams } from 'next/navigation';
 
 const Feedback = () => {
+  const { locale } = useParams();
+  const {
+    data: feedbackData,
+    isError,
+    isFetching,
+  } = useGetAllTestimonialsQuery();
+
   return (
     <section className="bg-bgWhite pb-[120px] pt-[120px]">
       <div className="container">
@@ -19,39 +27,55 @@ const Feedback = () => {
             <CarouselButton className="feedback-next-el" />
           </div>
         </div>
-        <Carousel
-          items={items}
-          prevEl=".feedback-prev-el"
-          nextEl=".feedback-next-el"
-          slidesPerView={1}
-          speed={500}
-          renderItem={(items) => {
-            return (
-              <div className="relative">
-                <div className="absolute right-[97px] top-[37px] h-[192px] w-[211px]">
-                  <Image
-                    src={'/icons/school/yellow-buddy-feedback.svg'}
-                    alt={'yellow buddy'}
-                    fill
-                    sizes="100%"
-                    className="-z-[1]"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  {items.map((item, idx) => (
-                    <FeedbackItem
-                      key={item.id}
-                      imgSrc={item.imgSrc}
-                      name={item.name}
-                      text={item.text}
-                      idx={idx}
+        {isFetching && <p className="container">Loading...</p>}
+        {isError && <p className="container">Something went wrong!</p>}
+        {!isFetching && feedbackData && (
+          <Carousel
+            items={feedbackData.data}
+            prevEl=".feedback-prev-el"
+            nextEl=".feedback-next-el"
+            slidesPerView={1}
+            speed={500}
+            renderItem={(items) => {
+              return (
+                <div className="relative">
+                  <div className="absolute right-[97px] top-[37px] h-[192px] w-[211px]">
+                    <Image
+                      src={'/icons/school/yellow-buddy-feedback.svg'}
+                      alt={'yellow buddy'}
+                      fill
+                      sizes="100%"
+                      className="-z-[1]"
                     />
-                  ))}
+                  </div>
+                  <div className="flex flex-col">
+                    {items.map((item, idx) => (
+                      <FeedbackItem
+                        key={item.id}
+                        imgSrc={item.imageUrl}
+                        name={
+                          locale === 'ua'
+                            ? item.nameUa
+                            : locale === 'en'
+                              ? item.nameEn
+                              : item.nameIt
+                        }
+                        text={
+                          locale === 'ua'
+                            ? item.reviewUa
+                            : locale === 'en'
+                              ? item.reviewEn
+                              : item.reviewIt
+                        }
+                        idx={idx}
+                      />
+                    ))}
+                  </div>
                 </div>
-              </div>
-            );
-          }}
-        />
+              );
+            }}
+          />
+        )}
       </div>
     </section>
   );
--- src/components/main/school/components/feedback/feedback-item/FeedbackItem.tsx
+++ src/components/main/school/components/feedback/feedback-item/FeedbackItem.tsx
@@ -11,7 +11,7 @@ interface FeedbackItemProps {
 
 const FeedbackItem = ({ idx, imgSrc, name, text }: FeedbackItemProps) => {
   return (
-    <div className="relative pl-[120px] pt-[60px] odd:self-start even:self-end">
+    <div className="relative pb-[32px] pl-[88px] pr-[24px] pt-[65px] odd:self-start even:self-end">
       <div className="absolute left-0 top-0 z-[2] w-[180px] text-center">
         <div
           className={clsx(
@@ -22,7 +22,7 @@ const FeedbackItem = ({ idx, imgSrc, name, text }: FeedbackItemProps) => {
           )}
         >
           <Image
-            className="object-contain"
+            className="object-cover"
             src={imgSrc}
             fill
             sizes="100%"
@@ -33,7 +33,7 @@ const FeedbackItem = ({ idx, imgSrc, name, text }: FeedbackItemProps) => {
           {name}
         </h4>
       </div>
-      <div className="relative py-16 pl-16 pr-10">
+      <div className="relative flex min-h-[220px] items-center justify-center py-[65px] pl-[105px] pr-[44px]">
         <Image
           src={'/images/feedback/text-bg.png'}
           alt={'background image'}
@@ -41,7 +41,47 @@ const FeedbackItem = ({ idx, imgSrc, name, text }: FeedbackItemProps) => {
           sizes="100%"
           className="-z-[1]"
         />
-        <p className="max-w-[550px] text-center">{text}</p>
+        {/* <div className="absolute -right-[23px] -top-[22px] h-[72px] w-[60px]">
+          <Image
+            src={'/icons/school/feedback-decoration.svg'}
+            alt={'background image'}
+            fill
+            sizes="100%"
+            className={clsx(
+              idx === 0 && '[&>svg]:fill-[#837ABB]',
+              idx === 1 && '[&>svg]:fill-[#F5E9CC]',
+              idx === 2 && '[&>svg]:fill-[#EA6D50]'
+            )}
+          />
+        </div> */}
+        <svg
+          className="absolute -right-[23px] -top-[22px] h-[72px] w-[60px]"
+          width="66"
+          height="75"
+          viewBox="0 0 66 75"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M13.5775 0.589445L65.8597 54.0796L52.791 74.2435L0.51386 20.7548L13.5775 0.589445Z"
+            fill={idx === 0 ? '#837ABB' : idx === 1 ? '#F5E9CC' : '#EA6D50'}
+          />
+        </svg>
+        <svg
+          className="absolute -bottom-[31px] -left-[25px] h-[72px] w-[60px]"
+          width="66"
+          height="75"
+          viewBox="0 0 66 75"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M13.5775 0.589445L65.8597 54.0796L52.791 74.2435L0.51386 20.7548L13.5775 0.589445Z"
+            fill={idx === 0 ? '#837ABB' : idx === 1 ? '#F5E9CC' : '#EA6D50'}
+          />
+        </svg>
+
+        <p className="max-w-[530px] text-center">{text}</p>
       </div>
     </div>
   );
