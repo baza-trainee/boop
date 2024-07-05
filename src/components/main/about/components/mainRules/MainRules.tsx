098648d1b55087a81c01e7edccb22641c1fdcb42
VickyBondarenko <quikyv@gmail.com>
2024-07-05 16:16:29 +0200

    Added translation text

--- src/components/main/about/components/mainRules/MainRules.tsx
+++ src/components/main/about/components/mainRules/MainRules.tsx
@@ -1,16 +1,14 @@
-import Logo from '@/components/main/footer/Logo/Logo';
 import Image from 'next/image';
 import React from 'react';
 
-// import { useTranslations } from 'next-intl';
-// import SectionTitle from '@/components/main/shared/SectionTitle';
+import { useTranslations } from 'next-intl';
 
 const MainRules = () => {
-  //   const t = useTranslations('About.team_structure');
+  const t = useTranslations('About.main_rules');
 
   return (
-    <section className="container mb-[120px] flex w-full flex-col gap-12 pt-[100px]">
-      <div className="flex items-start justify-center gap-[5px] pb-[70px]">
+    <section className="container mb-[120px] flex w-full flex-col gap-[70px] whitespace-pre-line pt-[100px]">
+      <div className=" flex items-start justify-center gap-[5px] ">
         <Image
           src="/images/heroSection/hero_logo.png"
           alt=""
@@ -19,12 +17,49 @@ const MainRules = () => {
           className="relative"
         />
         <h1 className="text-g title-gradient relative top-[25px] w-[570px] font-groppled text-[32px] font-bold leading-[120%]">
-          – робимо так, щоб дитинство тривало незалежно від обставин
+          {t('title')}
         </h1>
       </div>
-      <div>
-        <div></div>
-        <div></div>
+      <div className="relative flex items-start justify-between gap-6 font-raleway text-xl leading-normal text-textViolet">
+        <div className="flex flex-1  flex-col  gap-6">
+          <p className="w-[420px] pt-7">{t('rules.paragraph_1')}</p>
+          <Image
+            src="/images/mainRules/image_1.png"
+            alt="image_1"
+            width={960}
+            height={640}
+            className="relative"
+          />
+        </div>
+        <div className="flex flex-1 flex-col gap-6 ">
+          <div className="flex gap-6">
+            <Image
+              src="/images/mainRules/image_2.png"
+              alt="image_2"
+              width={306}
+              height={370}
+              className="min-h-[370px]"
+            />
+            <p className="w-[306px]">{t('rules.paragraph_2')}</p>
+          </div>
+          <div className="flex flex-col gap-6">
+            <Image
+              src="/images/mainRules/image_3.png"
+              alt="image_3"
+              width={2048}
+              height={1346}
+              className="relative"
+            />
+            <p className="w-[414px]">{t('rules.paragraph_3')}</p>
+          </div>
+        </div>
+        <Image
+          src="/images/mainRules/orange_man.svg"
+          alt="orange_man"
+          width={168}
+          height={164}
+          className="absolute bottom-0 left-[75px]"
+        />
       </div>
     </section>
   );
