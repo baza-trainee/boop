75cb7f277981c01682f488ccafe5177af4931fff
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2024-07-05 15:44:06 +0300

    added carousel Vertical To Partners, fixed Sttles Button

--- src/components/main/main-page/partners/Partners.tsx
+++ src/components/main/main-page/partners/Partners.tsx
@@ -1,47 +1,93 @@
 import React from 'react';
-// import { useTranslations } from 'next-intl';
+import { useTranslations } from 'next-intl';
 import { partnerItems } from './partnerItems';
 import PartnersCard from './partnersCard/PartnersCard';
+// import CarouselButton from '../../shared/carousel/carousel-button/CarouselButton';
+import { Carousel } from '../../shared/carousel/Carousel';
+import SectionTitle from '../../shared/SectionTitle';
+import VerticalCarouselButton from '../../shared/carousel/VerticalCarouselButton';
+
+
+
 
 const Partners = () => {
-  // const t = useTranslations('Press_about_us');
+  const t = useTranslations('Partners');
   return (
     <section className="pb-[120px] pt-[120px]">
-      <div className="flex flex-row w-full h-full items-center justify-center font-groppled text-textViolet">
-        <div className="flex flex-col bg-bgViolet py-[50px] pl-[80px] gap-[32px]">
+      <div className="flex flex-row  items-center justify-center">
+        <div className="w-full h-[600px] flex flex-col bg-bgViolet py-[50px] pl-[80px] gap-[32px]">
           <div className="flex gap-[10px] flex-row">
-            <p>Наші друзі</p>
-            <svg className="h-[1rem] w-[5rem]">
-              <use href="/icons/sprite.svg#title-line"></use>
-            </svg>
+          <SectionTitle title={t("title_1")}/>
           </div>
-          <div className='flex flex-wrap gap-[24px] w-[636px]'>
-            {partnerItems.map(({ url, name, img, width, height }, index) => (
-              <PartnersCard url={url} name={name} img={img} key={index} width={width} height={height} />
-            ))}
+          <div >
+          <Carousel
+              autoHeight={true}
+              loop={true}
+              items={partnerItems}
+              prevEl=".partners-prev-el"
+              nextEl=".partners-next-el"
+              slidesPerView={1}
+              direction="vertical"
+              renderItem={(partnerItems) => {
+                return(
+                <div className="relative flex flex-wrap gap-[24px] w-[636px]">
+                  {partnerItems.map((item) => (
+                    <PartnersCard
+                      key={item.id}
+                      imgSrc={item.imgSrc}
+                      name={item.name}
+                      id={item.id}
+                      width={item.width}
+                      height={item.height}
+                      url={item.url}
+                    />
+                    ))}
+                    <div className="absolute bottom-4 right-0 flex flex-col gap-[5px]">
+                    <VerticalCarouselButton  className="partners-prev-el" /> 
+                    <VerticalCarouselButton  className="partners-next-el" /> 
+                    </div>
+                </div>
+                )}}
+              />
           </div>
         </div>
-        <div className="flex flex-col bg-bgYellow py-[50px] pl-[80px] pr-[20px] gap-[32px]">
+        <div className="w-full h-[600px] flex flex-col bg-bgYellow py-[50px] pl-[80px] gap-[32px]">
           <div className="flex gap-[10px] flex-row">
-            <p>та партнери</p>
-            <svg className="h-[1rem] w-[5rem]">
-              <use href="/icons/sprite.svg#title-line"></use>
-            </svg>
+          <SectionTitle title={t("title_2")}/>
           </div>
-          <div className='flex flex-wrap gap-[24px] w-[636px]'>
-            {partnerItems.map(({ url, name, img, width, height }, index) => (
-              <PartnersCard url={url} name={name} img={img} key={index} width={width} height={height} />
-            ))}
+          <div >
+          <Carousel
+              autoHeight={true}
+              loop={true}
+              items={partnerItems}
+              prevEl=".partners-yeloow-prev-el"
+              nextEl=".partners-yellow-next-el"
+              slidesPerView={1}
+              direction="vertical"
+              renderItem={(partnerItems) => {
+                return(
+                <div className="relative flex flex-wrap gap-[24px] w-[636px]">
+                  {partnerItems.map((item) => (
+                    <PartnersCard
+                      key={item.id}
+                      imgSrc={item.imgSrc}
+                      name={item.name}
+                      id={item.id}
+                      width={item.width}
+                      height={item.height}
+                      url={item.url}
+                    />
+                    ))}
+                    <div className="absolute bottom-4 right-0 flex flex-col gap-[5px]">
+                    <VerticalCarouselButton  className="partners-yellow-prev-el" /> 
+                    <VerticalCarouselButton  className="partners-yellow-next-el" /> 
+                    </div>
+                </div>
+                )}}
+              />
           </div>
         </div>
-        {/* <div className="flex flex-col bg-bgYellow">
-          <div className="flex gap-[10px] flex-row p-[0px] pt-[70px] pl-[100px]">
-            <p>та партнери</p>
-            <svg className="h-[1rem] w-[5rem]">
-              <use href="/icons/sprite.svg#title-line"></use>
-            </svg>
-          </div>
-        </div> */}
+
       </div>
     </section>
   );
