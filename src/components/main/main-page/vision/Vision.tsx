6447c8ff52104cf49e5885ba446a441ddc2b96f0
VickyBondarenko <quikyv@gmail.com>
2024-07-06 16:26:07 +0200

    Added adaptive styles for vision section

--- src/components/main/main-page/vision/Vision.tsx
+++ src/components/main/main-page/vision/Vision.tsx
@@ -5,11 +5,11 @@ const Vision = () => {
   const t = useTranslations('Vision');
 
   return (
-    <section className="container mb-[120px] flex w-[1200px] flex-col items-center justify-center  gap-6 px-[77px] pb-6 text-center">
+    <section className="container flex w-full flex-col items-center justify-center gap-6 pb-6 text-center xs:mb-[74px]  md:mb-[88px] ml:mb-[100px]    xl:mb-[120px]">
       <h3 className="font-raleway text-xl font-bold  leading-[132%] text-textViolet">
         {t('subtitle')}
       </h3>
-      <h2 className="title-gradient font-groppled text-5xl font-bold leading-[124%]">
+      <h2 className="title-gradient font-groppled font-bold xs:text-[28px] xs:leading-[160%] md:text-[32px] ml:text-5xl    ml:leading-[124%] lg:w-[1056px]">
         {t('text')}
       </h2>
     </section>
