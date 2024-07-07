a9061b8da5d935459ece4d65e2dfde2511ef3ac9
VickyBondarenko <quikyv@gmail.com>
2024-07-07 13:22:14 +0200

    Added adaptive styles for goals section

--- src/components/main/main-page/goals/Goals.tsx
+++ src/components/main/main-page/goals/Goals.tsx
@@ -10,13 +10,16 @@ const Goals = () => {
   const goalsCards = GetGoalsCardsInfo();
 
   return (
-    <section className="mb-[120px] w-full  bg-bgViolet pb-[104px]  pt-[60px]">
-      <div className="container flex flex-col gap-8">
+    <section className="mb-[120px] w-full  bg-bgViolet pb-10 pt-10 md:pb-[50px] lg:pb-[60px] lg:pt-[60px] xl:pb-[70px] xl:pt-[80px] 2xl:pb-[104px]">
+      <div className="container flex flex-col gap-11">
         <SectionTitle title={t('title')} />
-        <ul className="flex w-full items-center justify-between">
+        <ul className="flex w-full flex-col items-center justify-center gap-[70px] md:flex-row md:flex-wrap md:justify-around md:gap-x-6 md:gap-y-[96px] lg:justify-between">
           {goalsCards.map((el, index) => {
             return (
-              <li key={index}>
+              <li
+                key={index}
+                className="flex w-[308px] justify-center lg:w-auto"
+              >
                 <GoalCard card={el} index={index} locale={locale} />
               </li>
             );
