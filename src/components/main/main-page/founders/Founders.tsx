8e32f9ac70bbeb8e67e10a134d350dd72a182ef1
VickyBondarenko <quikyv@gmail.com>
2024-07-09 22:38:15 +0200

    Fixed styles for founders image

--- src/components/main/main-page/founders/Founders.tsx
+++ src/components/main/main-page/founders/Founders.tsx
@@ -9,22 +9,40 @@ const Founders = () => {
   const isDesktop = useMediaQuery({
     query: '(min-width:1024px)',
   });
-  const isSmallScreen = useMediaQuery({
-    query: '(min-width:320px) and (max-width:1023px)',
+  const isLargeScreen = useMediaQuery({
+    query: '(min-width:1920px)',
   });
 
   return (
-    <section className="container  flex  max-w-screen-3xl   flex-col-reverse items-center justify-center gap-8 whitespace-pre bg-inherit xs:mb-[70px] md:mb-[88px] md:flex-row md:justify-between md:gap-[23px] ml:mb-[100px]  xl:mb-[120px] 4xl:justify-center 4xl:gap-0">
-      <div className="flex gap-[2px] ">
-        {isSmallScreen ? (
+    <section className="container  flex  max-w-screen-3xl   flex-col-reverse items-center justify-center gap-8 whitespace-pre bg-inherit xs:mb-[70px] md:mb-[88px] md:flex-row md:justify-between md:gap-[23px] ml:mb-[100px]  xl:mb-[120px] 4xl:justify-center 4xl:gap-[200px]">
+      {!isDesktop ? (
+        <Image
+          src="/images/founders_image_xs.png"
+          alt="Founders foto"
+          width={350}
+          height={443}
+          className=""
+        />
+      ) : (
+        <div className="relative ml:w-[513px] lg:w-fit">
           <Image
-            src="/images/founders_image_xs.png"
-            alt="Founders foto"
-            width={350}
-            height={443}
-            className=""
+            src="/images/founders_image.png"
+            alt="Founders_foto"
+            width={isLargeScreen ? 686 : 636}
+            height={isLargeScreen ? 782 : 724}
+            className=" "
           />
-        ) : (
+          <span className="absolute  bottom-0 right-0 h-4 w-[163px]  origin-top-left translate-x-[calc(100%+2px)] translate-y-full -rotate-90  text-left font-raleway text-[12px] font-semibold leading-[132%] text-textViolet">
+            {t('caption')}
+          </span>
+        </div>
+      )}
+      {/* {isDesktop && (
+      
+        )} */}
+
+      {/* <section className="container mx-auto mb-[120px] flex max-w-screen-3xl items-center justify-between whitespace-pre bg-inherit">
+        <div className="flex items-end gap-[2px]">
           <Image
             src="/images/founders_image.png"
             alt="Founders foto"
@@ -32,15 +50,25 @@ const Founders = () => {
             height={724}
             className=" "
           />
-        )}
-        {isDesktop && (
-          <span className="relative -bottom-4 h-4  w-[163px] origin-top-left -rotate-90 self-end text-left font-raleway text-[12px] font-semibold leading-[132%] text-textViolet">
+          <span className="relative -bottom-4  h-4 w-[163px] origin-top-left -rotate-90 text-left font-raleway text-[12px] font-semibold leading-[132%] text-textViolet">
             {t('caption')}
           </span>
-        )}
-      </div>
+        </div>
 
-      <div className="flex  flex-col justify-center gap-4 xs:w-full xs:pl-5 md:w-[309px] md:pl-0 ml:w-[359px] lg:w-[466px]  xl:w-[488px] 2xl:w-[526px] 4xl:w-[544px] ">
+        <div className="flex flex-col ">
+          <h3 className="font-raleway text-xl font-[500]  leading-[132%] text-mainViolet">
+            {t('subtitle')}
+          </h3>
+          <h2 className="title-gradient w-[500px] pb-4 font-groppled text-5xl font-bold leading-[110%]">
+            {t('title')}
+          </h2>
+          <p className="w-[350px] font-raleway text-xl font-bold leading-[132%] text-textViolet">
+            {t('text')}
+          </p>
+        </div>
+      </section> */}
+
+      <div className="flex  flex-col  gap-4  xs:pl-5  md:pl-0 ">
         <h3 className="font-raleway text-xl font-[500]  leading-[132%] text-mainViolet ">
           {t('subtitle')}
         </h3>
