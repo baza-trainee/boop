801cb7c4ae482c29d00d1df929fe50062921604d
VickyBondarenko <quikyv@gmail.com>
2024-07-10 17:03:26 +0200

    Added styles for xs screen

--- src/components/main/main-page/founders/Founders.tsx
+++ src/components/main/main-page/founders/Founders.tsx
@@ -37,36 +37,6 @@ const Founders = () => {
           </span>
         </div>
       )}
-      {/* {isDesktop && (
-      
-        )} */}
-
-      {/* <section className="container mx-auto mb-[120px] flex max-w-screen-3xl items-center justify-between whitespace-pre bg-inherit">
-        <div className="flex items-end gap-[2px]">
-          <Image
-            src="/images/founders_image.png"
-            alt="Founders foto"
-            width={636}
-            height={724}
-            className=" "
-          />
-          <span className="relative -bottom-4  h-4 w-[163px] origin-top-left -rotate-90 text-left font-raleway text-[12px] font-semibold leading-[132%] text-textViolet">
-            {t('caption')}
-          </span>
-        </div>
-
-        <div className="flex flex-col ">
-          <h3 className="font-raleway text-xl font-[500]  leading-[132%] text-mainViolet">
-            {t('subtitle')}
-          </h3>
-          <h2 className="title-gradient w-[500px] pb-4 font-groppled text-5xl font-bold leading-[110%]">
-            {t('title')}
-          </h2>
-          <p className="w-[350px] font-raleway text-xl font-bold leading-[132%] text-textViolet">
-            {t('text')}
-          </p>
-        </div>
-      </section> */}
 
       <div className="flex  flex-col  gap-4  xs:pl-5  md:pl-0 ">
         <h3 className="font-raleway text-xl font-[500]  leading-[132%] text-mainViolet ">
--- src/components/main/main-page/hero/Hero.tsx
+++ src/components/main/main-page/hero/Hero.tsx
@@ -2,57 +2,66 @@ import React from 'react';
 import Image from 'next/image';
 
 import { useLocale, useTranslations } from 'next-intl';
+import { useMediaQuery } from 'react-responsive';
 
 const Hero = () => {
   const t = useTranslations('Hero');
   const locale = useLocale();
+  const isMobile = useMediaQuery({
+    query: '(max-width:767px)',
+  });
 
   return (
-    <section className="container mx-auto max-w-screen-3xl box-border flex h-[876px] items-center justify-center bg-inherit">
+    <section className="container relative mx-auto box-border flex h-[645px] max-w-screen-3xl  flex-col items-center justify-center  pt-14">
       <Image
-        src="/images/hero-bg-image.svg"
+        src="/images/heroSection/hero_bg_xs.svg"
         alt=""
-        width={996}
-        height={1090}
-        className="absolute right-0 top-0 z-[2]"
+        width={390}
+        height={711}
+        className="absolute -top-2 right-0 z-[2] h-[711px] w-[390px] "
       />
-      <div className="relative h-[733px] w-[878px] items-center justify-center ">
+      {/* <div className="relative h-[733px] w-[878px] items-center justify-center ">
+        {!isMobile && (
+          <>
+            <Image
+              src="/images/heroSection/orange_circle.png"
+              alt=""
+              width={329}
+              height={329}
+              className="absolute left-0 top-[107px] z-0"
+            />
+
+            <Image
+              src="/images/heroSection/purpul_circle.png"
+              alt=""
+              width={213}
+              height={213}
+              className="absolute  bottom-0 right-[244px] z-10"
+            />
+            <Image
+              src="/images/heroSection/red_circle.png"
+              alt=""
+              width={213}
+              height={213}
+              className="absolute  right-0 top-0 z-10"
+            />
+          </>
+        )} */}
+      <div
+        className={` flex w-[247px] flex-col items-center justify-center ${locale === 'en' ? 'gap-3' : 'gap-6'} `}
+      >
         <Image
-          src="/images/heroSection/orange_circle.png"
+          src="/images/heroSection/hero_logo.png"
           alt=""
-          width={329}
-          height={329}
-          className="absolute left-0 top-[107px] z-0"
+          width={391}
+          height={227}
+          className="relative z-10 px-[5px]"
         />
-        <Image
-          src="/images/heroSection/purpul_circle.png"
-          alt=""
-          width={213}
-          height={213}
-          className="absolute  bottom-0 right-[244px] z-10"
-        />
-        <Image
-          src="/images/heroSection/red_circle.png"
-          alt=""
-          width={213}
-          height={213}
-          className="absolute  right-0 top-0 z-10"
-        />
-        <div
-          className={`flex flex-col items-center ${locale === 'en' ? 'gap-3' : 'gap-6'} pt-[172px]`}
-        >
-          <Image
-            src="/images/heroSection/hero_logo.png"
-            alt=""
-            width={391}
-            height={227}
-            className="relative z-10"
-          />
-          <h2 className="z-10 w-[275px] text-center font-groppled text-xl font-semibold leading-[132%] text-bgWhite">
-            {t('title')}
-          </h2>
-        </div>
+        <h2 className="z-10 w-[245px] text-center font-groppled text-xl font-semibold leading-[132%] text-bgWhite">
+          {t('title')}
+        </h2>
       </div>
+      {/* </div> */}
     </section>
   );
 };
