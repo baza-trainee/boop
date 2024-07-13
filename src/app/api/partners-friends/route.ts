6637fb84d868721b9cec2c9296cc385fd40d7e48
Oleh Hubskiy <gubskyolegod@gmail.com>
2024-07-13 17:27:34 +0300

    update partners

--- src/app/api/partners-friends/route.ts
+++ src/app/api/partners-friends/route.ts
@@ -31,10 +31,11 @@ export async function GET() {
 export async function POST(request: Request) {
   try {
     await prismaConnect();
-    const { logoUrl, link, section }: PartnersFriendsFormData =
+
+    const { logoUrl, logoId, link, section }: PartnersFriendsFormData =
       await request.json();
 
-    if (!logoUrl || !link || !section) {
+    if (!logoUrl || !link || !section || !logoId) {
       return NextResponse.json(
         { message: 'All fields are required' },
         { status: 400 }
@@ -53,6 +54,7 @@ export async function POST(request: Request) {
     const response = await prisma.partnersFriends.create({
       data: {
         logoUrl,
+        logoId,
         link,
         section,
       },
