c3286593e7a00a1e53f3b5bf9d85d1e7746e2c36
Богдан <sysoevbogdan82@gmail.com>
2024-07-23 00:01:27 +0300

    feat: added redux state for components

--- src/components/main/about/AboutPage.tsx
+++ src/components/main/about/AboutPage.tsx
@@ -4,6 +4,7 @@ import MainRules from './components/mainRules/MainRules';
 import Team from '../main-page/team/Team';
 import Blog from './components/blog/Blog';
 import Hospital_Clowning from './components/hospital_clowning/Hospital_Clowning';
+import Payment from '../payment/Payment';
 
 const AboutPage = () => {
   return (
@@ -12,6 +13,7 @@ const AboutPage = () => {
       <TeamStructure />
       <Team />
       <Hospital_Clowning />
+      <Payment />
       <Blog />
     </div>
   );
