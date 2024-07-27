bfd975706e02f2e5598c1bd9d63eeff429f314f7
Oleh Hubskiy <gubskyolegod@gmail.com>
2024-07-27 22:22:16 +0300

    fix:padding input

--- src/components/admin/ui/TextInput.tsx
+++ src/components/admin/ui/TextInput.tsx
@@ -32,7 +32,7 @@ const TextInput = forwardRef(function TextInput(
   }: TextInputProps,
   _ref: ForwardedRef<HTMLInputElement>
 ) {
-  const inputClassName = `relative w-full rounded-xl border p-2 text-sm placeholder:text-sm 
+  const inputClassName = `relative w-full rounded-xl border px-2 pt-2 pb-[23px] text-sm placeholder:text-sm 
       ${
         errorText
           ? 'border-red caret-red outline-red focus:outline-red'
