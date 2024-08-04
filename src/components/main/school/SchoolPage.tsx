b4e75f80afb0201b6314db3f11fb2ec06e1ccf6b
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2024-08-04 15:24:57 +0200

    fix: updated wave icon Introduction Section School pag

--- src/components/main/school/SchoolPage.tsx
+++ src/components/main/school/SchoolPage.tsx
@@ -19,7 +19,15 @@ const SchoolPage: React.FC = () => {
   const isLargeTablet = useMediaQuery({
     query: '(min-width: 1024px) and (max-width: 1279.98px)',
   });
-
+  const isLGDesktop = useMediaQuery({
+    query: '(min-width: 1280px) and (max-width: 1365.98px)',
+  });
+  const isXLDesktop = useMediaQuery({
+    query: '(min-width: 1366px) and (max-width: 1535.98px)',
+  });
+  const isLargeDesktop = useMediaQuery({
+    query: '(min-width: 1536px)',
+  });
   const [backgroundImage, setBackgroundImage] = useState<string>('');
 
   useEffect(() => {
@@ -29,15 +37,28 @@ const SchoolPage: React.FC = () => {
       setBackgroundImage("url('/icons/school/wave_md.svg')");
     } else if (isLargeTablet) {
       setBackgroundImage("url('/icons/school/wave_ml.svg')");
+    } else if (isLGDesktop) {
+      setBackgroundImage("url('/icons/school/wave_lg.svg')");
+    } else if (isXLDesktop) {
+      setBackgroundImage("url('/icons/school/wave_xl.svg')");
+    } else if (isLargeDesktop) {
+      setBackgroundImage("url('/icons/school/wave_4xl.svg')");
     } else {
       setBackgroundImage("url('/icons/school/wave.svg')");
     }
-  }, [isMobile, isTablet, isLargeTablet]);
+  }, [
+    isMobile,
+    isTablet,
+    isLargeTablet,
+    isLGDesktop,
+    isXLDesktop,
+    isLargeDesktop,
+  ]);
   return (
     <ModalProvider>
       <div className="bg-bgWhite">
         <section
-          className="bg-[right_bottom_200px] bg-no-repeat pb-[70px] pt-[72.5px] xs:bg-[right_bottom_74px] md:pb-[100px] md:pt-[100px] ml:bg-[right_bottom_10px] xl:pb-[120px] 3xl:bg-[right_top_35px] 3xl:pt-[120px]"
+          className="bg-[right_bottom_200px] bg-no-repeat pb-[70px] pt-[72.5px] xs:bg-[right_bottom_69px] md:bg-[right_4px_bottom_-32px] md:pb-[100px] md:pt-[100px] ml:bg-[right_bottom_10px] lg:bg-[right_-7px_bottom_50px] xl:bg-[right_bottom_-5px] xl:pb-[120px] 3xl:bg-[right_top_168px] 3xl:pt-[120px] 4xl:bg-[right_bottom_-13px]"
           style={{ backgroundImage }}
         >
           <div className="container mx-auto max-w-screen-3xl">
--- src/components/main/school/components/WhoCanJoin.tsx
+++ src/components/main/school/components/WhoCanJoin.tsx
@@ -8,10 +8,10 @@ const WhoCanJoin = () => {
 
   return (
     <>
-      <h2 className="title-gradient after:contetnt[''] mb-6 flex items-center justify-start gap-6 py-[2.5px] font-groppled text-2xl font-bold leading-[1.6] after:hidden after:h-[4px] after:w-[80px] after:bg-no-repeat after:py-1 md:py-[6px] md:text-[28px] ml:after:block ml:after:-translate-y-3/4 ml:after:bg-[url('/icons/school/title_line_desk.svg')]">
+      <h2 className="title-gradient after:contetnt[''] flex items-center justify-start gap-6 font-groppled text-2xl font-bold leading-[1.6] after:hidden after:h-[4px] after:w-[80px] after:bg-no-repeat after:py-1 md:text-[28px] ml:text-[32px] ml:after:block ml:after:-translate-y-3/4 ml:after:bg-[url('/icons/school/title_line_desk.svg')]">
         {t('join_title')}
       </h2>
-      <div className="relative flex items-end  justify-between">
+      <div className="relative flex items-end justify-between">
         <ul
           role="list"
           className="leading-norma pl-9 font-raleway text-base font-normal md:max-w-[331px] md:pl-8 md:text-lg ml:max-w-[481px] lg:max-w-[462px] lg:text-xl 3xl:max-w-[487px] 4xl:max-w-[602px] 4xl:pl-12"
@@ -19,8 +19,7 @@ const WhoCanJoin = () => {
           {textBlock.map((block, index) => (
             <li
               key={index}
-              className="relative mb-8 before:absolute before:left-[-36px] before:top-1/2 before:h-4 before:w-4 before:-translate-y-1/2 before:rounded-full
-          last:mb-0  before:odd:bg-violet before:even:bg-red md:before:left-[-32px] 4xl:before:left-[-48px]"
+              className="relative mb-8 before:absolute before:left-[-36px] before:top-1/2 before:h-4 before:w-4 before:-translate-y-1/2 before:rounded-full last:mb-0 before:odd:bg-violet before:even:bg-red md:before:left-[-32px] 4xl:before:left-[-48px]"
             >
               <p key={index} className="mb-4 text-textViolet last:mb-0">
                 {block.paragraph}
@@ -29,8 +28,8 @@ const WhoCanJoin = () => {
           ))}
         </ul>
         <div className="relative hidden h-[322px] w-[250px] md:block ml:h-[417px] ml:w-[328px] lg:h-[477px] lg:w-[396px] xl:h-[505px] xl:w-[484px] 3xl:h-[516px] 3xl:w-[526px] 4xl:h-[565px] 4xl:w-[544px]">
-          <div className="absolute bottom-0 h-[154px] w-[250px] bg-[#50439F] ml:h-[200px] ml:w-[328px] lg:h-[224px] lg:w-[396px] xl:h-[237px] xl:w-[484px] 3xl:h-[242px] 3xl:w-[525px] 4xl:h-[265px] 4xl:w-[544px]"></div>
-          <div className="absolute inset-0 left-1/2 h-[322px] w-[220px] -translate-x-1/2 bg-[url('/icons/school/man-smiling-with-fake-red-nose@2x.png')] bg-[length:220px_322px] ml:h-[417px] ml:w-[287px] ml:bg-[length:287px_417px]  lg:h-[477px] lg:w-[355px] lg:bg-[length:355px_477px] xl:h-[505px] xl:w-[357px] xl:bg-[length:357px_505px] 3xl:h-[516px]  3xl:w-[387px] 3xl:bg-[length:387px_516px] 4xl:h-[566px] 4xl:w-[401px]  4xl:bg-[length:401px_566px]"></div>
+          <div className="absolute bottom-0 h-[154px] w-[250px] bg-[#50439F] ml:h-[200px] ml:w-[328px] lg:h-[224px] lg:w-[396px] xl:h-[237px] xl:w-[484px] 3xl:h-[242px] 3xl:w-[526px] 4xl:h-[265px] 4xl:w-[544px]"></div>
+          <div className="absolute inset-0 left-1/2 h-[322px] w-[220px] -translate-x-1/2 bg-[url('/icons/school/man-smiling-with-fake-red-nose@2x.png')] bg-[length:220px_322px] ml:h-[417px] ml:w-[287px] ml:bg-[length:287px_417px] lg:h-[477px] lg:w-[355px] lg:bg-[length:355px_477px] xl:h-[505px] xl:w-[357px] xl:bg-[length:357px_505px] 3xl:left-[205px] 3xl:h-[516px] 3xl:w-[388px] 3xl:bg-[length:388px_516px] 4xl:h-[566px] 4xl:w-[401px] 4xl:bg-[length:401px_566px]"></div>
         </div>
       </div>
     </>
