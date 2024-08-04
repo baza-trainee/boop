61b5a6d120a53e03da0665a9f51da75041ae382e
cold <kholodihor@gmail.com>
2024-08-04 10:45:42 +0300

    fix(404): fix 404 logo

--- src/app/[locale]/[...notFound]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { CustomNotFound } from '@/components/main/notFound/CustomNotFound';
-import React from 'react';
-
-const NotFound = () => {
-  return <CustomNotFound />;
-};
-
-export default NotFound;
