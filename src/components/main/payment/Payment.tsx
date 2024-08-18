7611077744dfc4b28af38910af2554332f62a1ea
Богдан <sysoevbogdan82@gmail.com>
2024-08-18 14:59:45 +0300

    feat: added topLoader

--- src/components/main/payment/Payment.tsx
+++ src/components/main/payment/Payment.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SectionTitle from '../shared/SectionTitle';
-import Image from 'next/image';
+// import Image from 'next/image';
 import PaymentForm from './payment-form/PaymentForm';
 import { useTranslations } from 'next-intl';
 
@@ -10,27 +10,21 @@ const Payment = () => {
   return (
     <section
       id="donat"
-      className="mb-[120px] bg-beige py-[55px] max-xl:py-[80px] max-ml:py-[60px] max-sm:py-5"
+      className="mb-[120px] bg-beige py-5 md:py-[44px] ml:py-[60px] lg:py-[80px] 4xl:py-[60px]"
     >
       <div className="container">
-        <div className="flex justify-between gap-6 max-ml:flex-col max-ml:gap-12 max-md:gap-8">
-          <div className="text-right max-xl:text-center">
+        <div className="flex flex-col justify-between gap-6 md:gap-8 ml:gap-12 lg:flex-row lg:gap-6">
+          <div className="lg:max-w-[466px] xl:max-w-[488px] 3xl:max-w-[526px]">
             <SectionTitle
-              className="mb-8 max-md:mb-4 max-lg:[&>svg]:hidden max-ml:[&>svg]:block max-sm:[&>svg]:hidden"
+              // className="mb-8 max-md:mb-4 max-lg:[&>svg]:hidden max-ml:[&>svg]:block max-sm:[&>svg]:hidden"
+              className="mb-4 [&>svg]:hidden md:[&>svg]:block"
               title={t('title')}
             />
-            <div className="relative max-w-[540px] max-xl:max-w-[488px] max-lg:max-w-[466px] max-ml:flex max-ml:max-w-none">
-              <p className="text-gradient text-left text-xl font-semibold leading-[1.32] max-ml:max-w-[590px] max-md:max-w-[416px] max-custom:max-w-none">
+            <div className="relative">
+              {/* <p className="text-gradient text-left text-xl font-semibold leading-[1.32] max-ml:max-w-[590px] max-md:max-w-[416px] max-custom:max-w-none"> */}
+              <p className="text-gradient text-left text-[18px] font-semibold leading-[1.32] md:max-w-[416px] ml:max-w-[590px] lg:max-w-none lg:text-[20px]">
                 {t('text')}
               </p>
-              <div className="relative inline-block h-[172px] w-[232px] flex-shrink-0 max-xl:top-6 max-lg:top-2 max-ml:absolute max-ml:-right-[40px] max-ml:top-0 max-ml:h-[124px] max-ml:w-[167px] max-ml:translate-y-16 max-ml:-scale-x-1 max-md:right-3 max-custom:hidden xl:right-8">
-                <Image
-                  src="/icons/purple-buddy-payment.svg"
-                  fill
-                  sizes="100%"
-                  alt="purple man"
-                />
-              </div>
             </div>
           </div>
           <PaymentForm />
