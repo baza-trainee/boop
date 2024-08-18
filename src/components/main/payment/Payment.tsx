e38a2e370e8a4ba5ab51191f398a3024e9aa07d3
Богдан <sysoevbogdan82@gmail.com>
2024-08-18 16:53:22 +0300

    Update Payment.tsx

--- src/components/main/payment/Payment.tsx
+++ src/components/main/payment/Payment.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import SectionTitle from '../shared/SectionTitle';
-// import Image from 'next/image';
 import PaymentForm from './payment-form/PaymentForm';
 import { useTranslations } from 'next-intl';
 
@@ -16,12 +15,10 @@ const Payment = () => {
         <div className="flex flex-col justify-between gap-6 md:gap-8 ml:gap-12 lg:flex-row lg:gap-6">
           <div className="lg:max-w-[466px] xl:max-w-[488px] 3xl:max-w-[526px]">
             <SectionTitle
-              // className="mb-8 max-md:mb-4 max-lg:[&>svg]:hidden max-ml:[&>svg]:block max-sm:[&>svg]:hidden"
               className="mb-4 [&>svg]:hidden md:[&>svg]:block"
               title={t('title')}
             />
             <div className="relative">
-              {/* <p className="text-gradient text-left text-xl font-semibold leading-[1.32] max-ml:max-w-[590px] max-md:max-w-[416px] max-custom:max-w-none"> */}
               <p className="text-gradient text-left text-[18px] font-semibold leading-[1.32] md:max-w-[416px] ml:max-w-[590px] lg:max-w-none lg:text-[20px]">
                 {t('text')}
               </p>
