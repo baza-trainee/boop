553a1cedd045ec09e30394486915e7810ec2169e
Богдан <sysoevbogdan82@gmail.com>
2024-08-21 15:39:45 +0300

    fix: minor fixes for footer

--- src/components/main/footer/Footer.tsx
+++ src/components/main/footer/Footer.tsx
@@ -37,7 +37,7 @@ const Footer = () => {
       <DecoratedSvg />
       <AnimatedFooterMan
         className={
-          'top-41 absolute right-16 h-[112px] w-[115px] md:right-[40px] md:top-[23px] md:h-[168px] md:w-[164px] ml:-top-[12px] ml:right-[64px] lg:right-[165px] lg:top-[2px] 3xl:right-[285px] 4xl:right-[460px]'
+          'top-41 absolute right-16 hidden h-[112px] w-[115px] sm:block md:right-[40px] md:top-[23px] md:h-[168px] md:w-[164px] ml:-top-[12px] ml:right-[64px] lg:right-[165px] lg:top-[2px] 3xl:right-[285px] 4xl:right-[460px]'
         }
       />
       ;
