373ed1aea17ede8ce7b340e938d7fd64bad4bb11
cold <kholodihor@gmail.com>
2024-08-24 13:28:06 +0300

    fix: fix suspense error

--- src/app/[locale]/documents/[title]/page.tsx
+++ src/app/[locale]/documents/[title]/page.tsx
@@ -1,13 +1,7 @@
-import { Suspense } from 'react';
-import Loader from '@/components/shared/loader/Loader';
 import PdfViewer from '@/components/main/pdf-viewer/PdfViewer';
 
 const DocsPage = ({ params }: { params: { title: string } }) => {
-  return (
-    <Suspense fallback={<Loader />}>
-      <PdfViewer document={params.title} />
-    </Suspense>
-  );
+  return <PdfViewer document={params.title} />;
 };
 
 export default DocsPage;
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,5 @@
-import { Suspense } from 'react';
 import MainPage from '@/components/main/main-page/MainPage';
-import Loader from '@/components/shared/loader/Loader';
 
 export default function Home() {
-  return (
-    <Suspense fallback={<Loader />}>
-      <MainPage />
-    </Suspense>
-  );
+  return <MainPage />;
 }
