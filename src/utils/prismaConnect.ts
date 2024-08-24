28f1d02d4bbe7c6c8e6bd3c30a6f2a9e8dbb16a9
cold <kholodihor@gmail.com>
2024-08-24 19:05:04 +0300

    fix: remove prismaConnect

--- src/utils/prismaConnect.ts
+++
@@ -1,9 +0,0 @@
-import prisma from '@/lib/prisma';
-
-export async function prismaConnect() {
-  try {
-    await prisma.$connect();
-  } catch (error) {
-    return Error('Database connection failed');
-  }
-}
