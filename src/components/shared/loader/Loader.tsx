37fd573f961dce2db19fd14303487588f0546bfd
cold <kholodihor@gmail.com>
2024-08-27 11:34:47 +0300

    fix: change loader

--- src/components/shared/loader/Loader.tsx
+++ src/components/shared/loader/Loader.tsx
@@ -1,16 +1,19 @@
-import Image from 'next/image';
 import './loader.css';
 
 const Loader = () => {
   return (
-    <div className="backdrop-brightness-10 bg-yellowTransparent fixed left-0 right-0 top-0 z-[9999] flex h-full w-full flex-col items-center justify-center backdrop-blur-sm">
-      <Image
-        src="/images/loader.webp"
-        alt="loader"
-        width={120}
-        height={120}
-        className="loader min-h-[118px] min-w-[121px]"
-      />
+    <div className="backdrop-brightness-10 fixed left-0 right-0 top-0 z-[9999] flex h-full w-full flex-col items-center justify-center bg-yellowTransparent backdrop-blur-sm">
+      <div className="loading">
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+      </div>
     </div>
   );
 };
