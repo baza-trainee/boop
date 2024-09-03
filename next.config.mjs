864d0750c3dfef72d7ccb8f349c5b7f241ef7e2b
cold <kholodihor@gmail.com>
2024-09-03 11:35:13 +0300

    fix: fix counters on mobile view

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,49 @@
 /** @type {import('next').NextConfig} */
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
   reactStrictMode: false,
-  // output: 'standalone',
+  output: "standalone",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
       {
-        protocol: 'https',
-        hostname: 'maps.googleapis.com',
+        protocol: "https",
+        hostname: "maps.googleapis.com",
       },
     ],
   },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.node/,
-      use: 'raw-loader',
+      use: "raw-loader",
     });
-
     return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT,HEADERS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 };
 
