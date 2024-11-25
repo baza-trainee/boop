06a39b631189fcdaaafb84f376da50607a967d86
cold <kholodihor@gmail.com>
2024-11-25 16:12:40 +0200

    fix: fix docker

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,7 @@ const nextConfig = {
     ],
   },
   webpack: (config) => {
+    config.resolve.alias.canvas = false;
     config.module.rules.push({
       test: /\.node/,
       use: "raw-loader",
